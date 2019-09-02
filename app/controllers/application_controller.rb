845d53146b9a12ee2276dd86deaa6bc5a64c114d
kento <kozukata1993@gmail.com>
2019-09-02 23:17:05 +0900

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     month = Time.zone.now.month
     day = Time.zone.now.day
 
-    first_day = Time.zone.local(year, month, 1)
+    first_day = Time.zone.local(year, 12, 1)
     last_day = (first_day + 1.month) - 1.day 
 
     @reply_text = "Hello!!\n#{first_day}, #{last_day}"
