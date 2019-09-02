a1186fa2918a2324a145291f693e52828ac24451
kento <kozukata1993@gmail.com>
2019-09-02 23:03:32 +0900

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@ class ApplicationController < ActionController::Base
   before_action :make_reply
 
   def make_reply
+    year = Time.zone.now.year
+    month = Time.zone.now.month
+    day = Time.zone.now.day
+
+    first_day = Time.zone.local(year, month, 1)
+    last_day = (first_day << - 1) - 1
 
 
-
-    @reply_text = "Hello!!\n#{Time.zone.local(2019, 9, 2)}"
+    @reply_text = "Hello!!\n#{first_day}, #{last_day}"
   end
 
 end
